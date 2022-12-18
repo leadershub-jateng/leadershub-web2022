46de0818f439b320c3cce8bdecd849e83ddc47ba
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2022-12-18 08:57:11 +0700

    Update topic & article
    - add topic page & more article page
    - mod article page

--- article-page.php
+++ article-page.php
@@ -1,214 +1,220 @@
 <?php
-    require 'function.php';
-    $id = $_GET["id"];
-    $data_artikel = query("SELECT artikel.judul, artikel.gambar, artikel.penulis, artikel.tglUpload, artikel.isi, tag.deskripsi FROM artikel JOIN tag ON artikel.id_tag = tag.id_tag WHERE id_artikel = $id")[0];
+require 'function.php';
+$id = $_GET["id"];
+$data_artikel = query("SELECT artikel.judul, artikel.gambar, artikel.penulis, artikel.tglUpload, artikel.isi, tag.deskripsi FROM artikel JOIN tag ON artikel.id_tag = tag.id_tag WHERE id_artikel = $id")[0];
 
-    session_start();
-    if (empty($_SESSION['csrf_token'])) {
-    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-  }
+session_start();
+if (empty($_SESSION['csrf_token'])) {
+  $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Start Meta Data -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- End Meta Data -->
 
-    <!-- Start Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <!-- End Bootstrap -->
+<head>
+  <!-- Start Meta Data -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- End Meta Data -->
 
-    <!-- Start API Google font -->
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700&display=swap" rel="stylesheet">
-    <!-- End API Google font -->
+  <!-- Start Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <!-- End Bootstrap -->
 
-    <!-- Csrf Token -->
-    <meta name="csrf-token" content="<?= $_SESSION['csrf_token'] ?>">
+  <!-- Start API Google font -->
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700&display=swap" rel="stylesheet">
+  <!-- End API Google font -->
 
-    <!-- Start Style -->
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.33.1/sweetalert2.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <!-- End Style -->
+  <!-- Csrf Token -->
+  <meta name="csrf-token" content="<?= $_SESSION['csrf_token'] ?>">
 
-    <title>Leadershub Jawa Tengah</title>
-    <link rel="icon" href="img/icon.png">
+  <!-- Start Style -->
+  <link rel="stylesheet" href="style.css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.33.1/sweetalert2.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <!-- End Style -->
 
-  </head>
-  <body>
+  <title>Leadershub Jawa Tengah</title>
+  <link rel="icon" href="img/icon.png">
+
+</head>
+
+<body>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.33.1/sweetalert2.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script>
-    $(document).ready(function(){
+  <script>
+    $(document).ready(function() {
       //Mengirimkan Token Keamanan
       $.ajaxSetup({
-              headers : {
-                  'Csrf-Token': $('meta[name="csrf-token"]').attr('content')
-              }
-          });
-        
-      $('#form_komen').on('submit', function(event){
+        headers: {
+          'Csrf-Token': $('meta[name="csrf-token"]').attr('content')
+        }
+      });
+
+      $('#form_komen').on('submit', function(event) {
         event.preventDefault();
         let nama_pengirim = $('#nama_pengirim').val();
         let komen = $('#komen').val();
         swal.fire({
-              type: 'success',
-              title: 'Sukses...',
-              text: 'Komentar article sudah terkirim!'
-              });
-        
-        if(nama_pengirim==''){
-            swal.fire({
-              type: 'error',
-              title: 'Oops...',
-              text: 'Nama pengirim harus diisi!',
-              footer: '<a href="#">Why do I have this issue?</a>'
-              });
-        } else if(komen==''){
-            swal.fire({
-              type: 'error',
-              title: 'Oops...',
-              text: 'Komentar harus diisi!',
-              footer: '<a href="#">Why do I have this issue?</a>'
-              });
+          type: 'success',
+          title: 'Sukses...',
+          text: 'Komentar article sudah terkirim!'
+        });
+
+        if (nama_pengirim == '') {
+          swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: 'Nama pengirim harus diisi!',
+            footer: '<a href="#">Why do I have this issue?</a>'
+          });
+        } else if (komen == '') {
+          swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: 'Komentar harus diisi!',
+            footer: '<a href="#">Why do I have this issue?</a>'
+          });
         } else {
-            var form_data = $(this).serialize();
-            $.ajax({
-              url:"tambah_komentar.php",
-              method:"POST",
-              data:form_data,
-              success:function(data){
-                $('#form_komen')[0].reset();
-                $('#komentar_id').val('0');
-                load_comment();
-              }, error: function(data) {
-                      console.log(data.responseText)
-                    }
-            })
+          var form_data = $(this).serialize();
+          $.ajax({
+            url: "tambah_komentar.php",
+            method: "POST",
+            data: form_data,
+            success: function(data) {
+              $('#form_komen')[0].reset();
+              $('#komentar_id').val('0');
+              load_comment();
+            },
+            error: function(data) {
+              console.log(data.responseText)
+            }
+          })
         }
       });
- 
+
       load_comment();
- 
-      function load_comment(){
+
+      function load_comment() {
         $.ajax({
-          url:"ambil_komentar.php",
-          method:"POST",
-          success:function(data){
+          url: "ambil_komentar.php",
+          method: "POST",
+          success: function(data) {
             $('#display_comment').html(data);
-          }, error: function(data) {
-                  console.log(data.responseText)
-                }
+          },
+          error: function(data) {
+            console.log(data.responseText)
+          }
         })
       }
- 
-      $(document).on('click', '.reply', function(){
+
+      $(document).on('click', '.reply', function() {
         var komentar_id = $(this).attr("id");
         $('#komentar_id').val(komentar_id);
         $('#nama_pengirim').focus();
       });
     });
   </script>
-    <!-- START NAVBAR -->
-    <nav class="navbar navbar-expand-lg shadow-sm">
-      <div class="container-fluid container">
-        <a class="navbar-brand" href="#">
-          <img src="img/logo.png" alt="" width="auto" height="24" class="d-inline-block align-text-top">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav ms-auto nav-custom">
-            <a class="nav-link" aria-current="page" href="index.php">Home</a>
-            <a class="nav-link" href="about.html">About</a>
-            <a class="nav-link nav-active" href="article.php">Article</a>
-            <a class="nav-link" href="#">Program</a>
-          </div>
+  <!-- START NAVBAR -->
+  <nav class="navbar navbar-expand-lg shadow-sm">
+    <div class="container-fluid container">
+      <a class="navbar-brand" href="#">
+        <img src="img/logo.png" alt="" width="auto" height="24" class="d-inline-block align-text-top">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto nav-custom">
+          <a class="nav-link" aria-current="page" href="index.php">Home</a>
+          <a class="nav-link" href="about.html">About</a>
+          <a class="nav-link nav-active" href="article.php">Article</a>
+          <a class="nav-link" href="#">Program</a>
         </div>
       </div>
-    </nav>
-    <!-- END NAVBAR-->
+    </div>
+  </nav>
+  <!-- END NAVBAR-->
 
-    <!-- START BODY -->
-    <div class="container">
-      <div class="head-title pd-5 my-5">
-        <h1><?= $data_artikel["judul"] ?></h1>
+  <!-- START BODY -->
+  <div class="container">
+    <div class="head-title pd-5 my-5">
+      <h1><?= $data_artikel["judul"] ?></h1>
+    </div>
+    <div class="cover-img">
+      <img src="img/article/<?= $data_artikel["gambar"] ?>" alt="coverimage" width="100%">
+    </div>
+    <div class="content my-3 mb-5">
+      <div class="author my-4">
+        <h3><?= $data_artikel["penulis"] ?></h3>
+        <h6><?= $data_artikel["tglUpload"] ?> | <?= $data_artikel["deskripsi"] ?></h6>
       </div>
-      <div class="cover-img">
-          <img src="img/article/<?= $data_artikel["gambar"] ?>" alt="coverimage" width="100%">
-      </div>
-      <div class="content my-3 mb-5">
-        <div class="author my-4">
-          <h3><?= $data_artikel["penulis"] ?></h3>
-          <h6><?= $data_artikel["tglUpload"] ?> | <?= $data_artikel["deskripsi"] ?></h6>
-        </div>
-        <p><?= $data_artikel["isi"] ?></p>
-        <div class="container mb-3">
-          <h2 align="center" style="margin: 60px 10px 10px 10px;">Komentar Article</h2><hr>
-            <form method="POST" id="form_komen">
-              <div class="form-group">
-                <input type="text" name="nama_pengirim" id="nama_pengirim" class="form-control" placeholder="Masukkan Nama" />
-              </div>
+      <p><?= $data_artikel["isi"] ?></p>
+      <div class="container mb-3">
+        <h2 align="center" style="margin: 60px 10px 10px 10px;">Komentar Article</h2>
+        <hr>
+        <form method="POST" id="form_komen">
+          <div class="form-group">
+            <input type="text" name="nama_pengirim" id="nama_pengirim" class="form-control" placeholder="Masukkan Nama" />
+          </div>
           <div class="form-group">
             <textarea name="komen" id="komen" class="form-control" placeholder="Tulis Komentar" rows="5"></textarea>
           </div>
-        <div class="form-group">
-          <input type="hidden" name="komentar_id" id="komentar_id" value="0" />
-          <input type="submit" name="submit" id="submit" class="btn btn-info" value="Submit" />
-        </div>
+          <div class="form-group">
+            <input type="hidden" name="komentar_id" id="komentar_id" value="0" />
+            <input type="submit" name="submit" id="submit" class="btn btn-info" value="Submit" />
+          </div>
         </form>
-      <hr>
-     <h4 class="mb-3">Komentar :</h4>
-    <span id="message"></span>
-   
-      <div id="display_comment"></div>
-  </div>
+        <hr>
+        <h4 class="mb-3">Komentar :</h4>
+        <span id="message"></span>
+
+        <div id="display_comment"></div>
       </div>
     </div>
-    <!-- END BODY -->
+  </div>
+  <!-- END BODY -->
 
 
-    <!-- START FOOTER -->
-    <footer class="pt-5 pb-4 bg-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-3 col-md-4 col-sm-12 footer-content-custom footer-position">
-            <img class="logo-footer" src="img/logo-putih.png" alt="lhjateng" width="200px">
-          </div >
-          <div class="col-lg-7 col-md-5 col-sm-12 footer-content-custom footer-position">
-            <div class="col-lg-10">
-              <p>
-              Rumah bagi para komunitas dan organisasi kemahasiswaan yang ada di Jawa tengah untuk                bisa berjejaring, belajar, dan bertumbuh
-              </p>
-            </div>
-          </div> 
-          <div class="col-lg-2 col-md-3 col-sm-12 footer-content-custom footer-position">
-            <h6>Contact us:</h6>
-            <a href="mailto:...">
-              <img class="sosmed" src="img/gmail-logo.png" alt="lh-gmail" width="25px" > 
-            </a>
-            <a href="https://www.linkedin.com/company/leadershubjawatengah/" target="_blank">
-              <img class="sosmed" src="img/linkedin.png" alt="lh-linkedin" width="25px">
-            </a>
-            <a href="https://www.instagram.com/leadershubjateng/" target="_blank">
-              <img class="sosmed" src="img/instagram.png" alt="lh-instagram" width="25px" >
-            </a>
-          </div>         
+  <!-- START FOOTER -->
+  <footer class="pt-5 pb-4 bg-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-4 col-sm-12 footer-content-custom footer-position">
+          <img class="logo-footer" src="img/logo-putih.png" alt="lhjateng" width="200px">
+        </div>
+        <div class="col-lg-7 col-md-5 col-sm-12 footer-content-custom footer-position">
+          <div class="col-lg-10">
+            <p>
+              Rumah bagi para komunitas dan organisasi kemahasiswaan yang ada di Jawa tengah untuk bisa berjejaring, belajar, dan bertumbuh
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-12 footer-content-custom footer-position">
+          <h6>Contact us:</h6>
+          <a href="mailto:...">
+            <img class="sosmed" src="img/gmail-logo.png" alt="lh-gmail" width="25px">
+          </a>
+          <a href="https://www.linkedin.com/company/leadershubjawatengah/" target="_blank">
+            <img class="sosmed" src="img/linkedin.png" alt="lh-linkedin" width="25px">
+          </a>
+          <a href="https://www.instagram.com/leadershubjateng/" target="_blank">
+            <img class="sosmed" src="img/instagram.png" alt="lh-instagram" width="25px">
+          </a>
         </div>
       </div>
-    </footer>
-        <!-- END FOOTER -->
+    </div>
+  </footer>
+  <!-- END FOOTER -->
 
-    <!-- Start Script Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-    <!-- End Script Bootstrap -->
+  <!-- Start Script Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  <!-- End Script Bootstrap -->
 
-  </body>
+</body>
+
 </html>
---
+++ moreArticle.php
@@ -0,0 +1,176 @@
+<?php
+require 'function.php';
+$jumlahDataTiapHalaman = 6;
+$jumlahData = count(query("SELECT * FROM artikel"));
+$jumlahHalaman = ceil($jumlahData / $jumlahDataTiapHalaman);
+if (isset($_GET["halaman"])) {
+    $halamanAktif = $_GET["halaman"];
+} else {
+    $halamanAktif = 1;
+}
+$awalData = ($jumlahDataTiapHalaman * $halamanAktif) - 5;
+// halaman 1, awalData = 1
+// halaman 2, awalData = 7
+// halaman 3, awalData = 13
+
+
+$artikel = query("SELECT * FROM artikel ORDER BY tglUpload DESC LIMIT $awalData, $jumlahDataTiapHalaman");
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Start Meta Data -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- End Meta Data -->
+
+    <!-- Start Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <!-- End Bootstrap -->
+
+    <!-- Start API Google -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <!-- End API Google -->
+
+
+    <!-- Start Style -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="animate.css">
+    <!-- End Style -->
+
+    <title>Leadershub Jawa Tengah</title>
+    <link rel="icon" href="img/icon.png">
+    <!-- Start Animation On Scroll Style -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <!-- End Animation On Scroll Style -->
+</head>
+
+<body>
+    <!-- START PRE-LOADER -->
+    <?php include 'components/preloader.php'; ?>
+    <!-- END PRE-LOADER -->
+    <!-- START NAVBAR -->
+    <nav class="navbar navbar-expand-lg shadow-sm">
+        <div class="container-fluid container">
+            <a class="navbar-brand" href="index.php">
+                <img src="img/logo.png" alt="" width="auto" height="24" class="d-inline-block align-text-top">
+            </a>
+            <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ms-auto nav-custom">
+                    <a class="nav-link" href="index.php">Home</a>
+                    <a class="nav-link" href="about.php">About</a>
+                    <a class="nav-link nav-active" aria-current="page" href="article.php">Article</a>
+                    <a class="nav-link" href="program.php">Program</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- END NAVBAR -->
+
+    <!-- Start 1st Section -->
+    <div class="container py-5">
+        <div class="text-center" data-aos="fade-up" data-aos-duration="600" data-aos-delay="500">
+            <h1 class="heading">Article</h1>
+        </div>
+        <div class="row row-cols-1 row-cols-md-3 g-4 my-3">
+            <?php foreach ($artikel as $row) : ?>
+                <div class="col" data-aos="fade-up" data-aos-duration="600" data-aos-delay="500">
+                    <a href="article-page.php?id=<?= $row["id_artikel"] ?>" style="text-decoration: none;">
+                        <div class="card card-custom">
+                            <img src="img/article/<?= $row["gambar"] ?>" class="card-img-top card-img-custom">
+                            <div class="card-body">
+                                <h5 class="card-title-custom"><?= $row["judul"] ?></h5>
+                                <p class="card-author"><?= $row["tglUpload"] ?> | <?= $row["penulis"] ?></p>
+                                <h6 class="card-more">Baca lebih lanjut</h6>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            <?php endforeach ?>
+        </div>
+    </div>
+    <!-- End 1st Section -->
+
+    <!-- Start 2nd Section -->
+    <nav aria-label="Page navigation example" data-aos="zoom-in" data-aos-duration="600" data-aos-delay="00">
+        <ul class="pagination justify-content-center">
+            <?php if ($halamanAktif == 1) { ?>
+                <li class="page-item disabled">
+                    <a class="page-link">Previous</a>
+                </li>
+            <?php } else { ?>
+                <li class="page-item">
+                    <a class="page-link" href="?halaman=<?= $halamanAktif - 1; ?>">Previous</a>
+                </li>
+            <?php }; ?>
+            <?php for ($i = 1; $i <= $jumlahHalaman; $i++) { ?>
+                <?php if($i == 1) { ?>
+                    <li class="page-item"><a class="page-link" href="article.php"><?php echo $i ?></a></li>
+                <?php }else{ ?>
+                    <li class="page-item"><a class="page-link" href="?halaman=<?= $i; ?>"><?php echo $i ?></a></li>
+                <?php }; ?>
+            <?php }; ?>
+            <?php if ($halamanAktif == $jumlahHalaman) { ?>
+                <li class="page-item disabled">
+                    <a class="page-link">Next</a>
+                </li>
+            <?php } else { ?>
+                <li class="page-item">
+                    <a class="page-link" href="?halaman=<?= $halamanAktif + 1; ?>">Next</a>
+                </li>
+            <?php }; ?>
+
+        </ul>
+    </nav>
+    <!-- End 2nd Section -->
+
+    <!-- START FOOTER -->
+    <footer class="pt-5 pb-4 bg-footer mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-4 col-sm-12 footer-content-custom footer-position">
+                    <img class="logo-footer" src="img/logo-putih.png" alt="lhjateng" width="200px">
+                </div>
+                <div class="col-lg-7 col-md-5 col-sm-12 footer-content-custom footer-position">
+                    <div class="col-lg-10">
+                        <p>Rumah bagi para komunitas dan organisasi kemahasiswaan yang ada di Jawa tengah untuk bisa berjejaring, belajar, dan bertumbuh
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-12 footer-content-custom footer-position">
+                    <h6>Contact us:</h6>
+                    <a href="mailto:...">
+                        <img class="sosmed" src="img/gmail-logo.png" alt="lh-gmail" width="25px">
+                    </a>
+                    <a href="https://www.linkedin.com/company/leadershubjawatengah/" target="_blank">
+                        <img class="sosmed" src="img/linkedin.png" alt="lh-linkedin" width="25px">
+                    </a>
+                    <a href="https://www.instagram.com/leadershubjateng/" target="_blank">
+                        <img class="sosmed" src="img/instagram.png" alt="lh-instagram" width="25px">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </footer>
+    <!-- END FOOTER-->
+
+    <!-- Start Script Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <!-- End Script Bootstrap -->
+
+    <!-- Start Animation On Scroll (AOS) Library -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+    <!-- End Animation On Scroll (AOS) Library -->
+
+</body>
+
+</html>
