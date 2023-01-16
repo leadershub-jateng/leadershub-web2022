4294ea328ae3fe8cd965670ce9526da53c3420fa
Muhammad Afifudin <muhafifudin2306@gmail.com>
2023-01-16 14:16:19 +0700

    Edit and Delete Button, CKeditor for article page

--- admin/editArticle.php
+++ admin/editArticle.php
@@ -3,74 +3,86 @@ session_start();
 if (!isset($_SESSION['fname'])) {
   header("Location: login.php");
 }
-  require '../function.php';
-  $tagDB = query("SELECT * FROM `lhjateng`.`tag` LIMIT 1000;");
+require '../function.php';
+$tagDB = query("SELECT * FROM `lhjateng`.`tag` LIMIT 1000;");
 
-  $id = $_GET["id"];
-  $data_artikel = query("SELECT * FROM artikel WHERE id_artikel = $id")[0];
-  if(isset($_POST["submit"])){
-    if(edit($_POST) > 0){
-      echo "
+$id = $_GET["id"];
+$data_artikel = query("SELECT * FROM artikel WHERE id_artikel = $id")[0];
+if (isset($_POST["submit"])) {
+  if (edit($_POST) > 0) {
+    echo "
         <script>
           alert('data berhasil diubah!');
           document.location.href = 'postingan.php';
         </script>
       ";
-    }else{
-      echo "
+  } else {
+    echo "
         <script>
           alert('data gagal diubah!');
           document.location.href = 'postingan.php';
         </script>
       ";
-    }   
   }
+}
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
 
-<?php require '../components/admin/style.php';?>
+  <?php require '../components/admin/style.php'; ?>
 
-<!-- ======= Title Page ======== -->
-<title> Edit Post | Leadershub Jawa Tengah</title>
-<!-- ======= Title Page ======== -->
+  <!-- ======= Title Page ======== -->
+  <title> Edit Post | Leadershub Jawa Tengah</title>
+  <!-- ======= Title Page ======== -->
+  <script src="https://cdn.ckeditor.com/ckeditor5/35.4.0/classic/ckeditor.js"></script>
 
 </head>
-  <body>
- 
-      <!-- Layout wrapper -->
-    <div class="layout-wrapper layout-content-navbar">
-      <div class="layout-container">
+
+<body>
+
+  <!-- Layout wrapper -->
+  <div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+      <!-- ======= Navbar ======== -->
+      <?php include '../components/admin/menu.php'; ?>
+      <!-- ======= Navbar ======== -->
+
+      <!-- Layout container -->
+      <div class="layout-page">
         <!-- ======= Navbar ======== -->
-        <?php include '../components/admin/menu.php';?>
+        <?php include '../components/admin/navbar.php'; ?>
         <!-- ======= Navbar ======== -->
 
-        <!-- Layout container -->
-        <div class="layout-page">
-           <!-- ======= Navbar ======== -->
-           <?php include '../components/admin/navbar.php';?>
-            <!-- ======= Navbar ======== -->
+        <!-- ======= Content ======== -->
+        <?php include '../components/admin/posts/update.php'; ?>
+        <!-- ======= Content ======== -->
 
-          <!-- ======= Content ======== -->
-          <?php include '../components/admin/posts/update.php';?>
-            <!-- ======= Content ======== -->
-
-          </div>
-          <!-- ======= Footer ======== -->
-          <?php include '../components/admin/footer.php';?>
-            <!-- ======= Footer ======== -->
-          <!-- Content wrapper -->
-        </div>
-        
-        <!-- / Layout page -->
       </div>
-      
+      <!-- ======= Footer ======== -->
+      <?php include '../components/admin/footer.php'; ?>
+      <!-- ======= Footer ======== -->
+      <!-- Content wrapper -->
     </div>
-  
- <!-- ======= Script ======== -->
- <?php require '../components/admin/script.php';?>
+
+    <!-- / Layout page -->
+  </div>
+
+  </div>
+
+  <!-- ======= Script ======== -->
+  <?php require '../components/admin/script.php'; ?>
   <!-- ======= Script ======== -->
 
-  </body>
+  <script>
+    ClassicEditor
+      .create(document.querySelector('#editor'))
+      .catch(error => {
+        console.error(error);
+      });
+  </script>
+
+</body>
+
 </html>
